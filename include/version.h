2f47d77fb80ffc2973358212cc2e3d268cdeb8c8
roddo <roddo@gmail.com>
2026-01-22 13:41:37 -0500

    bump version

--- include/version.h
+++ include/version.h
@@ -1,5 +1,5 @@
 #include <config.h>
 
 #define sys_name "micro86"
-#define sys_release "0.01a"
-#define sys_version "micro86 version 0.01a"
+#define sys_release "0.02a"
+#define sys_version "micro86 version 0.02a"
